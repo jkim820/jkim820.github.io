cda83d9e4cae30e9e8a518e4e77a1b773dd41521
John Kim <jkim82092@gmail.com>
2019-05-08 14:43:29 -0400

    Fix header and remove menu icon w/ navpanel for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,32 +44,34 @@
 
 		// Title Bar.
 			$(
+				// '<div>' +
 				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
+					// '<a href="#navPanel" class="toggle"></a>' +
+					// '<span class="title">' + $('#logo').html() + '</span>' +
 					'<span class="title">' + $('#logo').html() + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
 
 		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+			// $(
+			// 	'<div id="navPanel">' +
+			// 		'<nav>' +
+			// 			$('#nav').navList() +
+			// 		'</nav>' +
+			// 	'</div>'
+			// )
+			// 	.appendTo($body)
+			// 	.panel({
+			// 		delay: 500,
+			// 		hideOnClick: true,
+			// 		hideOnSwipe: true,
+			// 		resetScroll: true,
+			// 		resetForms: true,
+			// 		side: 'left',
+			// 		target: $body,
+			// 		visibleClass: 'navPanel-visible'
+			// 	});
 
 	// Parallax.
 	// Disabled on IE (choppy scrolling) and mobile platforms (poor performance).
